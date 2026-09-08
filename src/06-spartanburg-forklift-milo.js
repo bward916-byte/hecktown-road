81dc4b95c4b7af05009242fef674ac1becd1be8f
bward916-byte <bward916@gmail.com>
2026-09-08 02:10:35 -0700

    source

--- src/06-spartanburg-forklift-milo.js
+++ src/06-spartanburg-forklift-milo.js
@@ -63,7 +63,7 @@ function drawSpartanburgPlay(g) {
 }
 WORLD_DEFS.spartanburg = {
   id: 'spartanburg', name: 'Spartanburg, SC', location: 'Spartanburg, South Carolina — Phillips DC', width: 4300, groundY: 440, truckX: 3960 - 420,
-  solids: () => [[0, 440, 4300, 200, 0], [1720, 392, 600, 48, 0], [1680, 416, 40, 24, 0], [3540, 288, 420, 92, 0], [3960, 328, 64, 54, 0], [3600, 410, 80, 30, 0], [3680, 380, 80, 60, 0], [3760, 350, 120, 90, 0]],
+  solids: () => [[0, 440, 4300, 200, 0], [1720, 392, 600, 48, 0], [1680, 416, 40, 24, 0], [3540, 288, 420, 92, 0], [3960, 328, 64, 54, 0], [3600, 410, 80, 30, 1], [3680, 380, 80, 60, 1], [3760, 350, 120, 90, 1]],
   lights: () => [{ x: 1370, y: 290, r: 260, color: [242, 181, 68], night: 1, glow: 1 }, { x: 1380, y: 380, r: 190, color: [255, 225, 170], night: 1 }, { x: 1592, y: 258, r: 160, color: [255, 200, 120], night: 1, cone: 1 }, { x: 1810, y: 330, r: 240, color: [255, 200, 120], night: 1, cone: 1 }, { x: 2130, y: 330, r: 240, color: [255, 200, 120], night: 1, cone: 1 }, { x: 3540 + 448, y: 380, r: 90, color: [255, 240, 200], night: 1 }],
   far: bakeSpartanburgFar, play: drawSpartanburgPlay,
   coffee: { x: 1100, y: 440 }, checkpoint: { x: 1060, y: 440 }, spawn: { x: 260, y: 440 }, weather: 2, setup: setupSpartanburg,
